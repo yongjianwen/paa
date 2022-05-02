9954506fdce33306564febf92e191a72cd442716
Jian Wen Yong (NCS) <jianwen.yong@ncs.com.sg>
2022-05-02 20:00:06 +0800

    Added imageUrls and comments in response

--- src/handlers/get-all-listings.js
+++ src/handlers/get-all-listings.js
@@ -19,13 +19,20 @@ exports.getAllListingsHandler = (event, context, callback) => {
       callback(err);
     } else {
       const req = new mssql.Request();
-      req.query('SELECT * FROM [Listing]')
+      req.query(`
+        SELECT
+        	*,
+        	(SELECT COUNT(*) FROM [AdoptionApplication] WHERE Listing = Listing.Id) AS ApplicationCount,
+        	(SELECT COUNT(*) FROM [ListingComment] WHERE Listing = Listing.Id) AS CommentCount,
+        	(SELECT COUNT(*) FROM [ListingLike] WHERE Listing = Listing.Id) AS LikeCount
+        FROM [Listing]
+      `)
         .then((result) => {
           let promises = result.recordset.map((res) => {
             return req.query('SELECT * FROM [User] WHERE Id = ' + res.ListedBy)
             .then((result2) => {
               res.user = result2.recordset;
-              return res
+              return res;
             })
             .catch((error2) => {
               console.log(error2);
@@ -33,9 +40,21 @@ exports.getAllListingsHandler = (event, context, callback) => {
           });
           
           Promise.all(promises).then((finalResult) => {
-            //console.log(finalResult);
-            mssql.close();
-            callback(null, finalResult);
+            let promises2 = finalResult.map((finalRes) => {
+              return req.query('SELECT * FROM [ListingImage] WHERE Listing = ' + finalRes.ID)
+              .then((result3) => {
+                finalRes.imageUrls = result3.recordset;
+                return finalRes;
+              })
+              .catch((error3) => {
+                console.log(error3);
+              });
+            });
+
+            Promise.all(promises2).then((finalFinalResult) => {
+              mssql.close();
+              callback(null, finalFinalResult);
+            });
           });
         })
         .catch((error) => {
--- src/handlers/get-listing-by-id.js
+++ src/handlers/get-listing-by-id.js
@@ -19,7 +19,14 @@ exports.getListingByIdHandler = (event, context, callback) => {
       callback(err);
     } else {
       const req = new mssql.Request();
-      req.query('SELECT * FROM [Listing] WHERE Id = ' + '1')
+      req.query(`
+        SELECT
+        	*,
+        	(SELECT COUNT(*) FROM [AdoptionApplication] WHERE Listing = Listing.Id) AS ApplicationCount,
+        	(SELECT COUNT(*) FROM [ListingComment] WHERE Listing = Listing.Id) AS CommentCount,
+        	(SELECT COUNT(*) FROM [ListingLike] WHERE Listing = Listing.Id) AS LikeCount
+        FROM [Listing] WHERE Id = 
+      ` + event['pathParameters']['listingId'].toString())
         .then((result) => {
           let promises = result.recordset.map((res) => {
             return req.query('SELECT * FROM [User] WHERE Id = ' + res.ListedBy)
@@ -33,9 +40,34 @@ exports.getListingByIdHandler = (event, context, callback) => {
           });
           
           Promise.all(promises).then((finalResult) => {
-            //console.log(finalResult);
-            mssql.close();
-            callback(null, finalResult);
+            let promises2 = finalResult.map((finalRes) => {
+              return req.query('SELECT * FROM [ListingImage] WHERE Listing = ' + finalRes.ID)
+              .then((result3) => {
+                finalRes.imageUrls = result3.recordset;
+                return finalRes;
+              })
+              .catch((error3) => {
+                console.log(error3);
+              });
+            });
+
+            Promise.all(promises2).then((finalFinalResult) => {
+              let promises3 = finalFinalResult.map((finalFinalRes) => {
+                return req.query('SELECT * FROM [ListingComment] WHERE Listing = ' + finalFinalRes.ID)
+                .then((result4) => {
+                  finalFinalRes.comments = result4.recordset;
+                  return finalFinalRes;
+                })
+                .catch((error4) => {
+                  console.log(error4);
+                });
+              });
+  
+              Promise.all(promises3).then((lastResult) => {
+                mssql.close();
+                callback(null, lastResult);
+              });
+            });
           });
         })
         .catch((error) => {
