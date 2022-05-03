dbcf12fda9aba5694d846ceea49945a7fd4051e0
Jian Wen Yong (NCS) <jianwen.yong@ncs.com.sg>
2022-05-03 16:11:36 +0800

    Added review-application.js

---
+++ src/handlers/review-application.js
@@ -0,0 +1,45 @@
+const mssql = require('mssql')
+
+const rdsUser = process.env.RDS_USER;
+const rdsPassword = process.env.RDS_PASSWORD;
+const rdsServer = process.env.RDS_SERVER;
+const rdsDatabase = process.env.RDS_DATABASE;
+
+exports.reviewApplicationHandler = (event, context, callback) => {
+  const config = {
+    user: rdsUser,
+    password: rdsPassword,
+    server: rdsServer,
+    database: rdsDatabase
+  };
+
+  console.log(event);
+  const userId = event.headers['userid'];
+  const applicationId = event['pathParameters']['applicationId'];
+  const rating = event.body['rating'];
+
+  mssql.connect(config, (err) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      const req = new mssql.Request();
+      req.query('UPDATE [AdoptionApplication] SET UserRating = ' + rating + ' WHERE Id = ' + applicationId)
+        .then((result) => {
+          mssql.close();
+          let response = {
+            statusCode: 200,
+            headers: {},
+            isBase64Encoded: false,
+            body: JSON.stringify(true)
+          };
+          callback(null, response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
+
+  return null;
+};
