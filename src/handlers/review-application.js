aa8d2a2dd7dc408081e73e08a32be1bbeaeeeec1
Jian Wen Yong (NCS) <jianwen.yong@ncs.com.sg>
2022-05-03 18:46:06 +0800

    Attempted to fix POST fetch issue

--- src/handlers/review-application.js
+++ src/handlers/review-application.js
@@ -16,7 +16,8 @@ exports.reviewApplicationHandler = (event, context, callback) => {
   console.log(event);
   const userId = event.headers['userid'];
   const applicationId = event['pathParameters']['applicationId'];
-  const rating = event.body['rating'];
+  const rating = JSON.parse(event.body['rating']);
+  console.log(rating);
 
   mssql.connect(config, (err) => {
     console.log('test1');
